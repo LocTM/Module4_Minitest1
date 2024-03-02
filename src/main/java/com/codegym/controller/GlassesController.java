c6777fcc04cb609578d92ee4aec112f0c36cc2c0
LocTM <tranminhloc.anhk@gmail.com>
2024-03-03 02:07:55 +0700

    add html file

--- src/main/java/com/codegym/controller/GlassesController.java
+++ src/main/java/com/codegym/controller/GlassesController.java
@@ -1,12 +1,71 @@
 package com.codegym.controller;
 
+import com.codegym.model.Glasses;
+import com.codegym.service.IGlassesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/glasses")
 public class GlassesController {
-    @GetMapping("/demo")
-    public String demo(){
-        return "index";
+    @Autowired
+    private IGlassesService glassesService;
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Glasses> glassesList = glassesService.findAll();
+        model.addAttribute("glasses", glassesList);
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("glasses", new Glasses());
+        return "/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Glasses glasses) {
+        glassesService.save(glasses);
+        return "redirect:/glasses";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String update(@PathVariable int id, Model model) {
+        model.addAttribute("glasses", glassesService.findById(id));
+        return "/update";
+    }
+
+    @PostMapping("/update")
+    public String update(Glasses glasses) {
+        glassesService.save(glasses);
+        return "redirect:/glasses";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("glasses", glassesService.findById(id));
+        return "/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Glasses glasses, RedirectAttributes redirect) {
+        glassesService.remove(glasses.getId());
+        redirect.addFlashAttribute("success", "Removed customer successfully!");
+        return "redirect:/glasses";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("glasses", glassesService.findById(id));
+        return "/view";
     }
 }
