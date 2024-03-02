550f0217c106fdd904e9d9c3a9dc6b0baa4a1d15
LocTM <tranminhloc.anhk@gmail.com>
2024-03-03 00:14:03 +0700

    add Bean templateEngine

--- src/main/java/com/codegym/configuration/AppConfiguration.java
+++ src/main/java/com/codegym/configuration/AppConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
@@ -24,12 +26,22 @@ public class AppConfiguration implements WebMvcConfigurer, ApplicationContextAwa
 
     @Bean
     public SpringResourceTemplateResolver templateResolver(){
-        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver().setApplicationContext(applicationContext);
         templateResolver().setPrefix("WEB-INF/views/");
         templateResolver().setSuffix(".html");
         templateResolver().setTemplateMode(TemplateMode.HTML);
         templateResolver().setCharacterEncoding("UTF-8");
-        return templateResolver();
+        return templateResolver;
     }
+
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+
 }
