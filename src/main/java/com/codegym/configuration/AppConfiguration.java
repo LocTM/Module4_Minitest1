e4056282c62212b4005b921f1ffda8a291e044b5
LocTM <tranminhloc.anhk@gmail.com>
2024-03-03 00:08:18 +0700

    add Bean templateResolver

--- src/main/java/com/codegym/configuration/AppConfiguration.java
+++ src/main/java/com/codegym/configuration/AppConfiguration.java
@@ -1,4 +1,35 @@
 package com.codegym.configuration;
 
-public class AppConfiguration {
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.codegym")
+public class AppConfiguration implements WebMvcConfigurer, ApplicationContextAware   {
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        templateResolver().setApplicationContext(applicationContext);
+        templateResolver().setPrefix("WEB-INF/views/");
+        templateResolver().setSuffix(".html");
+        templateResolver().setTemplateMode(TemplateMode.HTML);
+        templateResolver().setCharacterEncoding("UTF-8");
+        return templateResolver();
+    }
 }
